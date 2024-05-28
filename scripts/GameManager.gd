a392d1fa6c5f7b5fa9ae065fa0131774ca6e3d37
escobdan <escobdan1@gmail.com>
2024-05-27 23:38:33 -0600

    Adjusted killzone world borders and created pipesets

--- scripts/GameManager.gd
+++ scripts/GameManager.gd
@@ -1,1 +1,14 @@
 extends Node
+
+@onready var gameStarted : bool = false
+
+func _init():
+	print("WELCOME")
+	Engine.time_scale = 0
+	#print(get_viewport().size_2d_override)
+
+func _input(event):
+	if Input.is_action_just_pressed("flap") and !gameStarted:
+		gameStarted = true
+		Engine.time_scale = 1
+		print("Starting game...")
--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -2,4 +2,4 @@ extends Area2D
 
 func _on_body_entered(body):
 	print("You have died")
-	Engine.time_scale = 0.0
+	Engine.time_scale = 0
